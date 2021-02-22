f7c248883bfc0086a97446306f0e7d7ac8ae39b1
Panyaprach Tularak <panyaprach.jop@gmail.com>
2021-02-22 16:58:46 +0700

    Add request logging

---
+++ src/main/java/com/example/demo/config/RequestDiagnosticConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestDiagnosticConfiguration {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setIncludeClientInfo(true);
+
+        return filter;
+    }
+}
