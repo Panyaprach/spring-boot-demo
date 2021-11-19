4c23b66fb94bc321fbe8d93cc0d081e939ab0ae8
Panyaprach Tularak <panyaprach.jop@gmail.com>
2021-11-19 13:45:29 +0700

    control request rate

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.servlet.RateLimitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
+
+    @Autowired
+    @Lazy
+    private RateLimitInterceptor rateLimiter;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimiter);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/com/example/demo/servlet/RateLimitInterceptor.java
@@ -0,0 +1,71 @@
+package com.example.demo.servlet;
+
+import com.example.demo.exception.ExceptionDescriptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.bucket4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+@Component
+public class RateLimitInterceptor implements HandlerInterceptor {
+
+    private Bucket bucket;
+    @Autowired
+    private ObjectMapper mapper;
+
+    public RateLimitInterceptor() {
+        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1)));
+        this.bucket = Bucket4j.builder()
+                .addLimit(limit)
+                .build();
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
+
+        if (probe.isConsumed()) {
+            response.addHeader("X-Rate-Limit-Remaining", String.valueOf(probe.getRemainingTokens()));
+
+            return true;
+        } else {
+            long waitForRefill = NANOSECONDS.toSeconds(probe.getNanosToWaitForRefill());
+            ExceptionDescriptor descriptor = createDescription(request);
+            String json = mapper.writeValueAsString(descriptor);
+
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.addHeader("X-Rate-Limit-Retry-After-Seconds", String.valueOf(waitForRefill));
+            response.setStatus(descriptor.getStatus());
+            response.getWriter().write(json);
+
+            return false;
+        }
+    }
+
+    private ExceptionDescriptor createDescription(HttpServletRequest request) {
+        UrlPathHelper helper = new UrlPathHelper();
+        String reason = HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase();
+        int status = HttpStatus.TOO_MANY_REQUESTS.value();
+        String path = helper.getPathWithinApplication(request);
+        String message = "rate exceeded";
+
+        return ExceptionDescriptor.builder()
+                .withError(reason)
+                .withPath(path)
+                .withStatus(status)
+                .withMessage(message)
+                .build();
+    }
+}
