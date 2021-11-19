dd2b9a8a78bd1c01177f14ff8fded887a1a32be7
Panyaprach Tularak <panyaprach.jop@gmail.com>
2021-11-19 13:45:02 +0700

    customize objectmapper

---
+++ src/main/java/com/example/demo/config/JacksonConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.demo.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+}
