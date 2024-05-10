d69c66aa30fee91a9b80b5a7f4b54288d7f2e8f6
Panyaprach Tularak <panyaprach.jop@gmail.com>
2024-05-11 01:01:03 +0700

    add performance logging example

---
+++ src/main/java/com/example/demo/config/AopConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Aspect
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfiguration {
+
+    @Pointcut("within(com.example.demo.movie..*)")
+    public void monitor() {}
+
+    @Bean
+    public PerformanceMonitorInterceptor performanceMonitorInterceptor() {
+        return new PerformanceMonitorInterceptor(false);
+    }
+
+    @Bean
+    public Advisor performanceMonitorAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("com.example.demo.config.AopConfiguration.monitor()");
+        return new DefaultPointcutAdvisor(pointcut, performanceMonitorInterceptor());
+    }
+}
