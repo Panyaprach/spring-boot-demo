cdce54945f34b94a508f3e30b4cd565ad463f951
Panyaprach Tularak <panyaprach.jop@gmail.com>
2024-11-09 14:08:02 +0700

    supports asynchronous graphql resolver

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableAsync
 @EnableCaching
 @SpringBootApplication
 public class DemoApplication implements WebMvcConfigurer {
--- src/main/java/com/example/demo/movie/MovieQLController.java
+++ src/main/java/com/example/demo/movie/MovieQLController.java
@@ -13,26 +13,32 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class MovieQLController {
     @Autowired
     MovieService service;
 
+    @Async
     @SchemaMapping(typeName = "Query")
-    public Movie movie(@Argument String id) {
-        return service.findById(id);
+    public CompletableFuture<Movie> movie(@Argument String id) {
+        Movie movie = service.findById(id);
+
+        return CompletableFuture.completedFuture(movie);
     }
 
+    @Async
     @SchemaMapping(typeName = "Query")
-    public List<Movie> movies(@Argument MovieCriteria criteria,
-                              @Argument @NotNull @Positive @Valid Integer page,
-                              @Argument @NotNull @Positive @Valid @Max(100) Integer size
+    public CompletableFuture<List<Movie>> movies(@Argument MovieCriteria criteria,
+                                                 @Argument @NotNull @Positive @Valid Integer page,
+                                                 @Argument @NotNull @Positive @Valid @Max(100) Integer size
     ) {
         if (criteria == null)
             criteria = new MovieCriteria();
@@ -40,13 +46,15 @@ public class MovieQLController {
         Pageable paging = PageRequest.of(page - 1, size);
         Page<Movie> movies = service.findAll(criteria, paging);
 
-        return movies.toList();
+        return CompletableFuture.completedFuture(movies.toList());
     }
 
+    @Async
     @MutationMapping
-    public Movie movie(@Argument MovieInput input) {
+    public CompletableFuture<Movie> movie(@Argument MovieInput input) {
+        Movie movie = service.create(input.toMovie());
 
-        return service.create(input.toMovie());
+        return CompletableFuture.completedFuture(movie);
     }
 
     @SubscriptionMapping("newMovie")
